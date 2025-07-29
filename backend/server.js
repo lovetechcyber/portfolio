23922eddcd10c72d0787dc451a548fda300957fb
lovetechcyber <lovetechcyb@gmail.com>
2025-07-29 08:38:53 +0100

    done

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use('/api/contact', require('./routes/contactRoutes'));
-app.use('/api/auth', require('./routes/authRouts'));
+app.use('/api/auth', require('./routes/authRoutes'));
 // eslint-disable-next-line no-undef
 app.use('/api/blog', require('./routes/blogRoutes'));
 
